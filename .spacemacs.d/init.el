08f0162864df1446aca01e1bdf4589cb49610019
Matt Richard <matt@richardsoftware.com>
2016-01-11 22:09:30 -0600

    Fix emacs crash on startup
    I guess my local package loads too late so it tries to get that theme
    from melpa.  It doesn't exist there so it goes kaboom.  Not a fan of
    local packages so far...

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -97,8 +97,9 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(duotone
+   dotspacemacs-themes '(
                          material
+                         duotone
                          spacemacs-dark
                          spacemacs-light
                          solarized-light
