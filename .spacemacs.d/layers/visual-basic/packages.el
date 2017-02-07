d39fe946355c85b472cc49eb37d46d9263891712
Matt Richard <matt.richard@acadian.com>
2017-02-07 11:00:30 -0600

    narrow to vb function added.. todo: fix keybind

--- .spacemacs.d/layers/visual-basic/packages.el
+++ .spacemacs.d/layers/visual-basic/packages.el
@@ -4,9 +4,22 @@
     (visual-basic-mode :location local)
     ))
 
-(defun visual-basic/init-visual-basic-mode()
+(defun visual-basic-narrow-to-function ()
+  "Narrow to Function"
+  (interactive)
+  (save-excursion
+    (let ((beg (re-search-backward "^Function")))
+      (re-search-forward "End Function")
+      (end-of-line)
+      (narrow-to-region beg (point)))))
+
+(defun visual-basic/init-visual-basic-mode ()
   (use-package visual-basic-mode
     :defer t
     :mode ("\\.bas\\'\\|\\.frm\\'\\|\\.cls\\'\\|\\.tm\\'" . visual-basic-mode)
-    )
-  )
+    :config (progn
+              (spacemacs/set-leader-keys-for-major-mode 'visualbasic-mode
+                "nf" 'visual-basic-narrow-to-function)
+
+              (spacemacs/declare-prefix-for-mode 'visual-basic-mode "mn" "narrow"))))
+
